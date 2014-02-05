8920b11e16594009588e85fc81bbc29e09653dba
ubuntu <ubuntu@ubuntu.(none)>
2014-02-05 14:15:35 +0100

    new version based on fi-lab app template engine

---
+++ app/assets/javascripts/fi_lab_app/filab-style-app.js
@@ -0,0 +1,26 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require jquery
+//= require jquery_ujs
+//= require jquery.ui.all
+//= require modernizr
+//= require bootstrap
+//= require bootstrap-multiselect
+//= require bootstrap-scroll-modal
+//= require i18n
+//= require i18n/translations
+//= require flashy
+//= require colorfont
+//= require jquery-jvectormap-1.2.2.min.js
+//= require jquery-jvectormap-world-mill-en.js
+//= require_self

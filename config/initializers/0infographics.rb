cf83a4fd29577f63b35a9e2a88d24a6355b1f9bc
K4s14 <ubuntu@ubuntu.(none)>
2014-03-10 15:31:58 +0100

    Minor changes to index and error dispaly

--- config/initializers/0infographics.rb
+++ config/initializers/0infographics.rb
@@ -2,6 +2,7 @@ require_dependency 'fi_lab_infographics'
 
 FiLabInfographics.setup do |config|
   # Node.js proxy for monitoring
-  config.nodejs = 'http://localhost:1339'
+  config.nodejs = 'http://192.168.1.100:80'
+  config.nodata = 'No Data'
 
 end

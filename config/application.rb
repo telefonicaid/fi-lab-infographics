fe9a82cefbf44d5ee88b1ccb62ff1dccfb43c4b2
Federico Facca <federico.facca@gmail.com>
2014-04-02 12:53:48 +0200

    updates for conformancy with fi-ware styles

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module FiLabInfographics
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.initialize_on_precompile = true
+    config.assets.enabled = true
   end
 end

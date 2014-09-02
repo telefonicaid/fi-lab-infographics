3d4e63b02dd49f8b5832b27d02f9f1fec1d2734e
Federico M. Facca <federico.facca@gmail.com>
2014-09-02 17:59:16 +0200

    added additional config in lib files

--- lib/fi_lab_infographics.rb
+++ lib/fi_lab_infographics.rb
@@ -6,6 +6,7 @@ module FiLabInfographics
   mattr_accessor :jira_username
   mattr_accessor :jira_password
   mattr_accessor :jira_test
+  mattr_accessor :jira_success_message
 #   @@nodejs = 'http://192.168.1.100:80' 
   @@nodejs = 'http://193.205.211.69:1026' 
   @@timeout = 5
@@ -14,6 +15,7 @@ module FiLabInfographics
   @@jira_username = 'jira_username'
   @@jira_password = 'jira_password'
   @@jira_test = 1
+  @@jira_success_message = 'Thanks for reporting your issue. You will receive a notification for every update to the issue.'
 
   class << self
     def setup

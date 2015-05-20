93d6269a4347c897fd451b2116e4852556918991
Federico Facca <federico.facca@gmail.com>
2015-05-20 14:47:28 +0200

    version updated to new keystone APIs

--- config/initializers/0infographics.rb
+++ config/initializers/0infographics.rb
@@ -3,11 +3,18 @@ require_dependency 'fi_lab_infographics'
 FiLabInfographics.setup do |config|
   # Node.js proxy for monitoring
 #    config.nodejs = 'http://192.168.1.100:80'
-   config.nodejs = 'http://193.205.211.69:1026'
+#   config.nodejs = 'http://193.205.211.69:1026'
+#   config.nodejs = 'http://127.0.0.1:1336'
+#   config.nodejs = 'http://130.206.84.4:1028'
+   config.nodejs = 'http://130.206.84.4:1027'
+
+#   config.nodejs = 'http://10.0.64.4:1027'
    config.timeout = 5
    config.nodata = 'No Data'
    config.jira = 'http://jira.fi-ware.org'
-   config.jira_username = 'jira_username'
-   config.jira_password = 'jira_password'
-   config.jira_test = 1
+   config.jira_username = 'ext-usr'
+   config.jira_password = 'extUsrXiFi14'
+   config.jira_test = 0
+   config.jira_default_project = 'FIL'
+   config.jira_success_message = 'Thanks for reporting your issue. You will receive a notification for every update to the issue.'
 end

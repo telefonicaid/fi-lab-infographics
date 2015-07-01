f6161ac1f47154ffd5eb87df2157f36bdb3c2395
Federico Facca <federico.facca@gmail.com>
2015-07-01 13:26:32 +0200

    2015-07-01 AB Region mask

--- config/initializers/0infographics.rb
+++ config/initializers/0infographics.rb
@@ -6,14 +6,14 @@ FiLabInfographics.setup do |config|
 #   config.nodejs = 'http://193.205.211.69:1026'
 #   config.nodejs = 'http://127.0.0.1:1336'
 #   config.nodejs = 'http://130.206.84.4:1028'
-   config.nodejs = 'http://130.206.84.4:1027'
+   config.nodejs = 'http://130.206.84.4:11027'
 
 #   config.nodejs = 'http://10.0.64.4:1027'
    config.timeout = 5
    config.nodata = 'No Data'
    config.jira = 'http://jira.fi-ware.org'
-   config.jira_username = 'ext-usr'
-   config.jira_password = 'extUsrXiFi14'
+   config.jira_username = 'sla.fiwareops@gmail.com'
+   config.jira_password = 'Fiware@2015'
    config.jira_test = 0
    config.jira_default_project = 'FIL'
    config.jira_success_message = 'Thanks for reporting your issue. You will receive a notification for every update to the issue.'
